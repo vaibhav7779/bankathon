3b1820d9523accff623bf3b40603ca6305a1f749
Vaibhav Mangesh Lokhande <Vaibhav.Lokhande@freecharge.com>
2023-01-21 04:50:39 +0530

    Removed key

--- startlocal.sh
+++ startlocal.sh
@@ -4,9 +4,6 @@ export BROWSERSLIST_ENV=development
 
 ##### API CONFIG
 export DBAT_FE_API_BASE_URL=https://auth-api.dev.asldt.in
-# export DBAT_FE_API_KEY=PMAK-63233838659c773774691bd2-1060037c75eb17c3dabac26180e8b23266
-# export DBAT_FE_API_BASE_URL=http://localhost:3004
-# export DBAT_FE_OAUTH_BASE_URL=https://oauth.api.dev.axisb.com
 export DBAT_FE_API_TIMEOUT=30000
 export DBAT_FE_DISABLE_CRPTOGRAPHY=true
 # export DBAT_FE_ACCESS_TOKEN_REQUEST_HEADER_KEY='X-Access-Token'
